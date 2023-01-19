12b02564660f713221fd0790c51887e1b1de42ae
Dgarzona <dgarzona@sannicolas.com.sv>
2023-01-19 17:18:23 -0600

    first commit

---
+++ Self-Version/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Self_Version
+{
+    internal class Program
+    {
+        #region ocultar consola
+        [System.Runtime.InteropServices.DllImport("kernel32.dll")]
+        static extern IntPtr GetConsoleWindow();
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+        static int SW_HIDE = 0;
+        static int SW_SHOW = 5;
+        #endregion
+
+        static void Main(string[] args)
+        {
+            //ocultar consola
+            var handle = GetConsoleWindow();
+            ShowWindow(handle, SW_HIDE);
+            try
+            {
+                //declaracion de variables
+                string DirFrom = @"E:\extensiones\Ext", DirTo = @"E:\extensiones\Latest", DirPrev = @"E:\extensiones\Previus";
+                List<string> FilesFrom, FilesTo;
+
+                //agarra todas las extensiones
+                var applist = Logic.ReadJson();
+
+                //verifica que exista la carpeta para colocar 
+                Logic.verifyDirectory(DirFrom);
+
+                //Verificar que la carpeta tenga algo
+                FilesFrom = Logic.verifyFile(DirFrom);
+                if (!FilesFrom.Any())
+                    return;
+
+                //verifica que exista la carpeta para colocar 
+                Logic.verifyDirectory(DirTo);
+
+                //ingresa las nuevas extensiones a la carpeta
+                foreach (string File in FilesFrom)
+                {
+                    FileInfo mFile = new FileInfo(File);
+                    if (!new FileInfo(DirTo + @"\" + mFile.Name).Exists)
+                        mFile.MoveTo(DirTo + @"\" + mFile.Name);
+                }
+
+                //recibe todo dentro de la lista de latest
+                FilesTo = Logic.verifyFile(DirTo);
+
+                //organizar las carpetas posteriores
+                Logic.Organizing(FilesTo, applist, DirTo, DirPrev);
+            }
+            catch (Exception ex)
+            {
+                ShowWindow(handle,SW_SHOW);
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ Self-Version/logic.cs
@@ -0,0 +1,87 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Self_Version
+{
+    internal static class Logic
+    {     
+        /// <summary>
+        /// lee las configuraciones del json con todas las extensiones que existen
+        /// </summary>
+        /// <returns>la lista de extensiones existente</returns>
+        public static List<Apps> ReadJson()
+        {
+            List<Apps> list = new List<Apps>();
+
+            using (StreamReader r = new StreamReader(@"E:\extensiones\Config\CheckApps.json"))
+            {
+                string json = r.ReadToEnd();
+                list = JsonConvert.DeserializeObject<List<Apps>>(json);
+            }
+            return list;
+        }
+        /// <summary>
+        /// verifica si existe el directorio, sino, lo crea
+        /// </summary>
+        /// <param name="directory">path a consultar</param>
+        public static void verifyDirectory(string directory)
+        {
+            if (!Directory.Exists(directory))
+            {
+                Directory.CreateDirectory(directory);
+            }
+        }
+        /// <summary>
+        /// recibe las rutas para crear una  lista de todos los archivos
+        /// </summary>
+        /// <param name="directory">direccion a extraer archivos</param>
+        /// <returns>lista de archivos en el directorio</returns>
+        public static List<string> verifyFile(string directory) 
+        {
+            var files =  Directory.GetFiles(directory);
+            return files.ToList();
+        }
+        /// <summary>
+        /// Funcion  de organizacion de carpetas despues de latest
+        /// </summary>
+        /// <param name="LatestApps">lista de archivos existentes en la carpeta</param>
+        /// <param name="AllExtensions">lista de extensiones en total</param>
+        /// <param name="latestPath">parametro de la carpeta de las ultimas versiones</param>
+        /// <param name="PreviusPath">parametro de la carpeta de las versiones previas</param>
+        public static void Organizing(List<string> LatestApps, List<Apps> AllExtensions,string latestPath,string PreviusPath)
+        {
+            string tempPath; 
+            foreach(Apps App in AllExtensions)
+            {
+                var TempList = (from apps in LatestApps
+                           where apps.Contains(App.name)
+                           select apps).ToList();
+
+                if (TempList.Count > 1)
+                {
+                    //ordena para que el mas nuevo este primero
+                    TempList.Sort();
+                    TempList.Reverse();
+
+                    //verifica la creacion de carpetas de versiones previas
+                    verifyDirectory(PreviusPath + @"\" + App.name);
+                    tempPath = PreviusPath + @"\" + App.name;
+
+                    //itera para poder mover las apps a un carpeta
+                    for (int i = 1; i < TempList.Count; i++)
+                    {
+                        FileInfo mFile = new FileInfo(TempList[i]);
+                        if (!new FileInfo(PreviusPath + @"\" + mFile.Name).Exists)
+                            mFile.MoveTo(tempPath + @"\" + mFile.Name);
+                    }
+                }
+            }
+        }
+    }
+}
